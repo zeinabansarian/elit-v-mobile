287d9a49f1514c1f7e611c775819b5c921fc56ea
Zeinab <zeinabansarian6280@gmail.com>
2024-07-29 16:25:41 +0330

    complete section 6 of about page

--- js/aboutUs mobile.js
+++ js/aboutUs mobile.js
@@ -2,27 +2,19 @@ let aboutNav = document.querySelectorAll(".aboutNav ul li")
 
 
   
-  const lenis = new Lenis()
+  // const lenis = new Lenis()
+
   
-  
-  function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
-  }
-  
-  requestAnimationFrame(raf)
-  
-  
-  aboutNav[0].classList.add("activeNav")
-  for (let i = 0; i < aboutNav.length; i++) {
-      const element = aboutNav[i];
-      element.addEventListener("click" , function (params) {
+  // aboutNav[0].classList.add("activeNav")
+  // for (let i = 0; i < aboutNav.length; i++) {
+  //     const element = aboutNav[i];
+  //     element.addEventListener("click" , function (params) {
         
-              $(".aboutNav ul li").removeClass("activeNav")
-              element.classList.add("activeNav")
-              lenis.scrollTo(`.scroll-${i+1}` , {
-                  offset : -122,   
-              })
+  //             $(".aboutNav ul li").removeClass("activeNav")
+  //             element.classList.add("activeNav")
+  //             lenis.scrollTo(`.scroll-${i+1}` , {
+  //                 offset : -122,   
+  //             })
           
-      })
-  }
+  //     })
+  // }
