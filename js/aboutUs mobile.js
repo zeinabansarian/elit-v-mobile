807a0e2f037ec07ba27e5df7bdfaf1217e3c725b
Zeinab <zeinabansarian6280@gmail.com>
2024-07-29 17:03:52 +0330

    add some change

--- js/aboutUs mobile.js
+++ js/aboutUs mobile.js
@@ -2,19 +2,28 @@ let aboutNav = document.querySelectorAll(".aboutNav ul li")
 
 
   
-  // const lenis = new Lenis()
-
+  const lenis = new Lenis()
   
-  // aboutNav[0].classList.add("activeNav")
-  // for (let i = 0; i < aboutNav.length; i++) {
-  //     const element = aboutNav[i];
-  //     element.addEventListener("click" , function (params) {
+  // ScrollTrigger.normalizeScroll(true);
+
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+  
+  requestAnimationFrame(raf)
+  
+  
+  aboutNav[0].classList.add("activeNav")
+  for (let i = 0; i < aboutNav.length; i++) {
+      const element = aboutNav[i];
+      element.addEventListener("click" , function (params) {
         
-  //             $(".aboutNav ul li").removeClass("activeNav")
-  //             element.classList.add("activeNav")
-  //             lenis.scrollTo(`.scroll-${i+1}` , {
-  //                 offset : -122,   
-  //             })
+              $(".aboutNav ul li").removeClass("activeNav")
+              element.classList.add("activeNav")
+              lenis.scrollTo(`.scroll-${i+1}` , {
+                  offset : -75,   
+              })
           
-  //     })
-  // }
+      })
+  }
